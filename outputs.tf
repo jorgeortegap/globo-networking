1f024adcfae6686e8c7c029b8c7e990191fdd7a5
Jorge Ortega <jorgeortegap@gmail.com>
2026-05-14 17:06:11 -0400

    Switch to using tfe outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
 ##################################################################################
 # OUTPUT
 ##################################################################################
+
+output "vpc_id" {
+  value       = module.main_vpc.vpc_id
+  description = "VPC ID"
+}
+
+output "public_subnets" {
+  value       = module.main.public_subnets
+  description = "List of public subnets"
+}
